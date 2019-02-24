60404c6d2055decd99363cedd1d68745eada30a6
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-02-25 02:13:37 +0400

    TODO://add specific object for changing values ob object in iterator

---
+++ pages/posts/HtmlFilesFromMdFiles.js
@@ -0,0 +1,24 @@
+const { AsyncObject } = require('@cuties/cutie')
+const showdown  = require('showdown')
+const path = require('path')
+
+class HtmlFilesFromMdFiles extends AsyncObject {
+  constructor(mdFiles, htmlDir, fileNames) {
+    super(mdFiles, htmlDir, fileNames)
+  }
+
+  syncCall() {
+    return (mdFiles, htmlDir, fileNames) => {
+      const htmlFiles = {}
+      const converter = new showdown.Converter()
+      Object.keys(mdFiles).forEach((mdPath, index) => {
+        const mdContent = mdFiles[mdPath]
+        const htmlContent = converter.makeHtml(mdContent)
+        htmlFiles[path.join(htmlDir, `${fileNames[index].split('.')[0]}.html`)] = htmlContent
+      })
+      return htmlFiles
+    }
+  }
+}
+
+module.exports = HtmlFilesFromMdFiles
