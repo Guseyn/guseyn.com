c2387c3d8356530f41246706cc04662d642d45f3
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-02-28 08:30:26 +0400

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@ set -e
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add production ssh://5.187.7.231/guseyn.com
+echo "config ok"
+git remote add production ssh://root@5.187.7.231/guseyn.com
+echo "remote add ok"
 git push production master # push our updates
+echo "push ok"
