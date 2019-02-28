aba107de52890b57779d66914973be10fc08170b
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-02-28 08:07:28 +0400

    1.0.0

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git config --global push.default simple # we only want to push one branch — master
+# specify the repo on the live server as a remote repo, and name it 'production'
+# <user> here is the separate user you created for deploying
+git remote add production ssh://5.187.7.231/guseyn.com
+git push production master # push our updates
---
+++ run.sh
@@ -0,0 +1,5 @@
+ssh root@5.187.7.231 bash -c "'
+cd guseyn.com
+npm install @page-libs/cli
+page br
+'"
