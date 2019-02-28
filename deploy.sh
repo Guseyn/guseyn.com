59ab750b83d06f78f32799e4916d32e0c821d870
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-02-28 12:34:33 +0400

    fix remote ssh script

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@ ssh root@5.187.7.231 bash -c "'
 if [ ! -d guseyn.com ]
 then
   git clone https://github.com/Guseyn/guseyn.com.git guseyn.com
+  cd guseyn.com
 else
   cd guseyn.com
   git pull
 fi
-npm install @page-libs/cli
+npm install @page-libs/cli -g
+npm install
 pkill -f node
 page br
 '"
