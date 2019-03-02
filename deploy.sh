12a77e60bcf3ccafc614171085ee371c808203dc
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-03-02 15:46:53 +0400

    install page cli + change prod config

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ ssh bitnami@3.9.27.159 bash -c "'
 if [ ! -d guseyn.com ]
 then
   git clone https://github.com/Guseyn/guseyn.com.git guseyn.com
+  npm install @page-libs/cli -g
   cd guseyn.com
 else
   cd guseyn.com
