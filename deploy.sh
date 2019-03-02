9b45b60fb6b9e66f5da638f304d3c0db15513089
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-03-02 17:48:37 +0400

    manually using nvm :( //TOOO: improve in the future

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,6 @@ ssh ubuntu@3.8.218.230 bash -c "'
 if [ ! -d guseyn.com ]
 then
   git clone https://github.com/Guseyn/guseyn.com.git guseyn.com
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-  nvm --version
-  nvm install --lts
-  nvm use --lts
   npm install @page-libs/cli -g
   cd guseyn.com
 else
@@ -19,5 +15,9 @@ page b prod
 '"
 
 # if needed
+#  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+#  nvm --version
+#  nvm install --lts
+#  nvm use --lts
 # npm install npm -g
 # npm install @page-libs/cli -g
