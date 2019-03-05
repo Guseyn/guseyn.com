1cb5ff2ce0a085f41448eb5bbd03f112ac864505
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-03-05 15:47:40 +0400

    #36 #103

--- static/js/es6/main/main.js
+++ static/js/es6/main/main.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const DayNightButton = require('./DayNightButton')
+const { ResponseFromAjaxRequest, ResponseBody } = require('@page-libs/ajax')
+const { ElementWithInnerHTML } = require('@page-libs/dom')
+const { HtmlFromMd } = require('@page-libs/md2html')
 
 /* eslint-disable no-new */
 window.onload = () => {
@@ -11,4 +14,32 @@ window.onload = () => {
   new DayNightButton(
     document.getElementById('day-night')
   )
+  const cutiesDetailsBox = document.getElementById('cuties-details')
+  if (cutiesDetailsBox) {
+    new ElementWithInnerHTML(
+      cutiesDetailsBox,
+      new HtmlFromMd(
+        new ResponseBody(
+          new ResponseFromAjaxRequest(
+            { url: '/../../md/cuties-details.md', method: 'GET' }
+          )
+        ),
+        { 'tables': true }
+      )
+    ).call()
+  }
+  const pageLibsDetailsBox = document.getElementById('page-libs-details')
+  if (pageLibsDetailsBox) {
+    new ElementWithInnerHTML(
+      pageLibsDetailsBox,
+      new HtmlFromMd(
+        new ResponseBody(
+          new ResponseFromAjaxRequest(
+            { url: '/../../md/page-libs-details.md', method: 'GET' }
+          )
+        ),
+        { 'tables': true }
+      )
+    ).call()
+  }
 }
