193a1e319c4ae7a8cfa55b297e99c7e811dab7a4
guseyn <guseynism@gmail.com>
2019-03-17 12:45:46 +0400

    1.0.65

--- pages/BaseTemplateWrapper.js
+++ pages/BaseTemplateWrapper.js
@@ -24,6 +24,7 @@ class BaseTemplateWrapper extends AsyncObject {
               new Head(
                 new Meta('charset="UTF-8"'),
                 new Meta('name="viewport" content="width=device-width, initial-scale=1"'),
+                new Meta('name="google-site-verification" content="vGxE5xshQhWEvbfiGVWZ4qmfLx_1WW8P82ZW0RP0mwg"'),
                 new Title('fan of yours'),
                 new Link('rel="shortcut icon" type="image/png" href="/../image/favicon.png"'),
                 new Style('https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:400,400i,700,700i', 'type="text/css"'),
