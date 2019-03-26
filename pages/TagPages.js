183f632488362f58882306d2e80153d13f8ebf0b
guseyn <guseynism@gmail.com>
2019-03-26 11:25:29 +0400

    little fixes

--- pages/TagPages.js
+++ pages/TagPages.js
@@ -4,14 +4,14 @@ const { AsyncObject } = require('@cuties/cutie')
 const cheerio = require('cheerio')
 const path = require('path')
 
-class TagsFromPosts extends AsyncObject {
+class TagPages extends AsyncObject {
   constructor (posts, dirToSave, version) {
     super(posts, dirToSave, version)
   }
 
   syncCall () {
     return (posts, dirToSave, version) => {
-      const tags = {}
+      const tagPages = {}
       const postCount = {}
       Object.keys(posts).forEach((postPath) => {
         const postContent = posts[postPath]
@@ -21,18 +21,18 @@ class TagsFromPosts extends AsyncObject {
         $('a.tag').each((index, elm) => {
           const tagName = $(elm).text()
           const key = path.join(dirToSave, `${tagName.replace(/ /g, '').toLowerCase()}.html`)
-          tags[key] = tags[key] || `<div class="posts-by-tag">0 posts about ${tagName}</div>`
+          tagPages[key] = tagPages[key] || `<div class="posts-by-tag">0 posts about ${tagName}</div>`
           postCount[key] = postCount[key] || 0
-          tags[key] += `<div class="post-link"><a href="${postLink}">${title}</a></div>`
+          tagPages[key] += `<div class="post-link"><a href="${postLink}">${title}</a></div>`
           postCount[key] += 1
         })
       })
-      Object.keys(tags).forEach((path) => {
-        tags[path] = tags[path].replace('0 posts about', `${postCount[path]} ${postCount[path] === 1 ? 'post' : 'posts'} about`)
+      Object.keys(tagPages).forEach((path) => {
+        tagPages[path] = tagPages[path].replace('0 posts about', `${postCount[path]} ${postCount[path] === 1 ? 'post' : 'posts'} about`)
       })
-      return tags
+      return tagPages
     }
   }
 }
 
-module.exports = TagsFromPosts
+module.exports = TagPages
