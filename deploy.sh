23bbcc00bbcabba014b4723afba64358f71265f8
guseyn <guseynism@gmail.com>
2019-04-01 16:04:34 +0400

    and another attempt

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,18 @@ ssh root@209.97.128.89 bash -c "'
 if [ ! -d guseyn.com ]
 then
   git clone https://github.com/Guseyn/guseyn.com.git guseyn.com
+  git config --global user.email "guseynism@gmail.com"
   npm install @page-libs/cli -g
   cd guseyn.com
-  npm install
+  npm install --no-optional
   mkdir logs
 else
   cd guseyn.com
   git fetch --all
   git reset --hard origin/master
   git pull
-  npm install
-  page b
+  npm install --no-optional
+  page b prod
   git add --all
   git commit -m "local build changes"
 fi
