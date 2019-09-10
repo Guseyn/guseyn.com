dd88856808b423c0d256963c93801341e0f84a9b
guseyn <guseynism@gmail.com>
2019-09-10 13:48:42 +0400

    echo

---
+++ server/endpoints/EchoEndpoint.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const {
+  EndedResponse,
+  WrittenResponse,
+  ResponseWithWrittenHead
+} = require('@cuties/http')
+const {
+  Endpoint,
+  RequestBody
+} = require('./../index')
+
+class EchoEndpoint extends Endpoint {
+  constructor (regexpUrl, type) {
+    super(regexpUrl, type)
+  }
+
+  body (request, response) {
+    //  Use RequestBody object for fetching body from request
+    return new EndedResponse(
+      new WrittenResponse(
+        new ResponseWithWrittenHead(
+          response, 200, 'ok', {
+            'Content-Type': 'application/json'
+          }
+        ), new RequestBody(request)
+      )
+    )
+  }
+}
+
+module.exports = EchoEndpoint
