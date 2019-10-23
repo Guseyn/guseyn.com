ee12356da4e6a83f9947115b50151387d20fe0bf
guseyn <guseynism@gmail.com>
2019-10-23 09:26:57 +0400

    full chain ssl

--- remote-create-ssl.sh
+++ remote-create-ssl.sh
@@ -6,7 +6,7 @@ killall -s KILL node
 cd /opt/letsencrypt
 sudo -H ./letsencrypt-auto certonly --standalone --renew-by-default -d guseyn.com -d www.guseyn.com
 cd
-cp /etc/letsencrypt/live/guseyn.com/cert.pem guseyn.com/cert.pem
+cp /etc/letsencrypt/live/guseyn.com/fullchain.pem guseyn.com/cert.pem
 cp /etc/letsencrypt/live/guseyn.com/privkey.pem guseyn.com/key.pem
 cd guseyn.com
 page r prod &
