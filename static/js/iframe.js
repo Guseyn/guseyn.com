08e0f00aa3407aab3968630da73457f26cea136c
guseyn <guseynism@gmail.com>
2019-11-20 00:09:11 +0400

    little fixes

--- static/js/iframe.js
+++ static/js/iframe.js
@@ -2,9 +2,9 @@
 
 /* eslint-disable no-unused-vars */
 window.makeIframesResizable = (className, proportion) => {
-  var iframes = document.getElementsByClassName(className)
+  const iframes = document.getElementsByClassName(className)
   window.addEventListener('resize', (event) => {
-    for (var i = 0; i < iframes.length; i++) {
+    for (let i = 0; i < iframes.length; i++) {
       if (iframes[i]) {
         iframes[i].width = iframes[i].parentElement.offsetWidth
         iframes[i].height = iframes[i].parentElement.offsetWidth / proportion
