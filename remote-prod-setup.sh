806f567a87e4940a8a667a0dfafb56b9d9d7f630
guseyn <guseyn@guseyn.com>
2023-02-11 23:54:38 +0200

    setup2

--- remote-prod-setup.sh
+++ remote-prod-setup.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 ssh -i guseyn_rsa root@46.101.16.208 bash -c "'
-if [ ! -d guseyn ]
+if [ ! -d guseyn.com ]
 then
   git clone git@github.com:Guseyn/guseyn.com.git
   git config --global user.email \"guseyn@guseyn.com\"
-  cd guseyn
+  cd guseyn.com
   npm install
-  mkdir logs
-  npm run guseyn-prod-setup
+  npm run guseyn:prod:setup
 else
-  cd guseyn
+  cd guseyn.com
   git fetch --all
   git reset --hard origin/master
   git pull
   npm install
-  npm run guseyn-prod-setup
+  npm run guseyn:prod:setup
 fi
 '"
