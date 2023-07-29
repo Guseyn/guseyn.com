a845d57f95fbc7756282806af6b9f92d7935558d
guseyn <guseyn@guseyn.com>
2023-07-29 14:40:18 +0300

    delete turbo links

--- remote-update-lets-encrypt-token.sh
+++ remote-update-lets-encrypt-token.sh
@@ -2,5 +2,5 @@
 ssh -i guseyn_rsa root@46.101.16.208 bash -c "'
 cd guseyn.com
 rm lets-encrypt-token
-echo "ItPeh2tGkAZisHn32SmoYq0G_5LgCaZkRU_dkuBJk9U" > lets-encrypt-token
+echo "$1" > lets-encrypt-token
 '"
