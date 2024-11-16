d1e467ad3b4ffc21dfd91effc3c84730a4f9b1d1
guseyn <guseyn@guseyn.com>
2024-11-16 15:28:51 +0400

    update docker compose

--- nodes/setupFileLogging.js
+++ nodes/setupFileLogging.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
+const { execSync } = require('child_process')
 
 module.exports = function setupFileLogging(logFile) {
+  execSync(`touch ${logFile}`)
   const logFileStream = fs.createWriteStream(logFile, { flags: 'a' })
   global.log = function log(...message) {
     logFileStream.write(`${new Date().toISOString()} - worker (pid:${process.pid}) - ${message}\n`);
