3c877cb1d59297949e978d296316340cf3a9815d
guseyn <guseyn@guseyn.com>
2024-11-16 18:50:01 +0400

    update

--- remote-prod-run.sh
+++ remote-prod-run.sh
@@ -4,7 +4,16 @@ cd guseyn.com
 git fetch --all
 git reset --hard origin/master
 git pull
-npm install
 pkill node
+
+# Remove all containers
+docker rm -f \$(docker ps -aq) || echo "No containers to remove"
+
+# Remove unused volumes
+docker volume prune -f || echo "No volumes to prune"
+
+# Remove all images
+docker rmi -f \$(docker images -q) || echo "No images to remove"
+
 DOMAIN=guseyn.com,www.guseyn.com DOMAIN_EMAIL=guseyn@guseyn.com docker-compose -f docker-compose.prod.yml up --build --force-recreate --remove-orphans
 '"
