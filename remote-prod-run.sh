6d5fc30bcc7a6bc19370bdf4979403394378155f
guseyn <guseyn@guseyn.com>
2024-11-16 18:25:40 +0400

    update

--- remote-prod-run.sh
+++ remote-prod-run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-ssh -i guseyn_rsa root@46.101.16.208 bash -c "'
+ssh -i guseyn_rsa root@174.138.15.193 bash -c "'
 cd guseyn.com
 git fetch --all
 git reset --hard origin/master
 git pull
 npm install
 pkill node
-npm run guseyn:prod:start
+docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -q)
+DOMAIN=guseyn.com,www.guseyn.com DOMAIN_EMAIL=guseyn@guseyn.com docker-compose -f docker-compose.prod.yml up --build --force-recreate --remove-orphans
 '"
