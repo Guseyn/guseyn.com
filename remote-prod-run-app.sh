4a8e4cffed830bd3d22738f6e758a1b08cd8d86e
guseyn <guseyn@guseyn.com>
2024-11-20 00:09:35 +0400

    update

--- remote-prod-run-app.sh
+++ remote-prod-run-app.sh
@@ -6,5 +6,14 @@ git reset --hard origin/master
 git pull
 pkill node
 
+# Remove all containers
+docker rm -f \$(docker ps -aq) || echo "No containers to remove"
+
+# Remove unused volumes
+docker volume prune -f || echo "No volumes to prune"
+
+# Remove all images
+docker rmi -f \$(docker images -q) || echo "No images to remove"
+
 docker-compose -f docker-compose.prod.app.yml up --build
 '"
