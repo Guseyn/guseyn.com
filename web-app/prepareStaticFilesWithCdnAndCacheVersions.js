b2e863b424cf46ca3be9feddf960ccb8297c10cd
guseyn <guseyn@guseyn.com>
2024-11-20 00:14:23 +0400

    update

--- web-app/prepareStaticFilesWithCdnAndCacheVersions.js
+++ web-app/prepareStaticFilesWithCdnAndCacheVersions.js
@@ -5,6 +5,7 @@ const removeCdnFromUrls = require('./../nodes/removeCdnFromUrls')
 const environment = process.env.ENV
 
 module.exports = async function prepareStaticFilesWithCdnAndCacheVersions() {
+  await removeCdnFromUrls('web-app/static', 'https://cdn.guseyn.com')
   await updateCacheVersionsInUrls('web-app/static')
   if (environment === 'prod') {
     await addCdnToUrls('web-app/static', 'https://cdn.guseyn.com')
