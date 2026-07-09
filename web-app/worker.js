7d7a86898f5878a3b0666a47752918480620e7d9
guseyn <guseyn@guseyn.com>
2026-07-10 02:29:41 +0400

    update

--- web-app/worker.js
+++ web-app/worker.js
@@ -34,7 +34,7 @@ server(
     indexFile: './web-app/static/html/all-posts.html', 
     api: [],
     static: [
-      src(/^\/((html\/main-page-template\.html)|(html\/frammento-main-page-template\.html)|(html\/content)|css|js|json|svg|image|md|pdf|font|ttf|yml|magenta-soundfonts)/, {
+      src(/^\/((html\/main-page-template\.html)|(html\/frammento-main-page-template\.html)|(html\/content\/)|css|js|json|svg|image|md|pdf|font|ttf|yml|magenta-soundfonts)/, {
         baseFolder,
         useGzip: true,
         useCors,
