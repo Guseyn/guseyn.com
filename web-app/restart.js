786a53f65e4b2f7f77f28b3b24ddd3b7aaa34988
guseyn <guseyn@guseyn.com>
2026-07-10 00:42:38 +0400

    update

--- web-app/restart.js
+++ web-app/restart.js
@@ -4,22 +4,46 @@ const primaryProcessId = fs.readFileSync('primary.pid', 'utf-8')
 const version = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version
 const environment = process.env.ENV
 
-import prepareStaticFilesWithCdnAndCacheVersions from './prepareStaticFilesWithCdnAndCacheVersions'
+/* ──────────────────────────────────────────────────────────────── */
+/* 🌍  ENVIRONMENT CHECK                                            */
+/* ──────────────────────────────────────────────────────────────── */
+const environment = process.env.ENV
 
-prepareStaticFilesWithCdnAndCacheVersions().then(() => {
-   // It does not kill main process, it just sends a user signal to it to restart its workers
-  process.kill(primaryProcessId, 'SIGUSR1')
+// Enable cache versioning for any environment except local
+const weUpdateCacheVersionsInUrls = environment !== 'local'
 
-  console.log(
-  `
-  We just sent SIGUSR1 to the primary process with pid: ${primaryProcessId}.
-  Version: ${version}, environment: ${environment}.
+/* ──────────────────────────────────────────────────────────────── */
+/* 🔄  UPDATE STATIC FILE VERSION QUERIES                            */
+/* ──────────────────────────────────────────────────────────────── */
+if (weUpdateCacheVersionsInUrls) {
+  const logFile = './output.log'
 
-  Then primary process will send message to its subprocesses to exit with code 0.
-  It will restart them (gracefully and with timeout one by one).
-  That will allow to reach zero downtime while we restarting the application with new codebase (everything in worker.js).
+  // Store logs into output.log so it doesn't pollute stdout
+  setupFileLogging(logFile)
 
-  P.S.: If you need to update primary.js as well, you need to shutdown whole application and run it again.
-  `
-  )
-})
+  // Apply ?v=<hash> to all static resource URLs
+  await updateCacheVersionsInUrls('web-app/static')
+}
+
+await removeCdnFromUrls('web-app/static', 'https://cdn.guseyn.com')
+if (environment === 'prod') {
+  await addCdnToUrls('web-app/static', 'https://cdn.guseyn.com')
+} else {
+  await removeCdnFromUrls('web-app/static', 'https://cdn.guseyn.com')
+}
+
+// It does not kill main process, it just sends a user signal to it to restart its workers
+process.kill(primaryProcessId, 'SIGUSR1')
+
+console.log(
+`
+We just sent SIGUSR1 to the primary process with pid: ${primaryProcessId}.
+Version: ${version}, environment: ${environment}.
+
+Then primary process will send message to its subprocesses to exit with code 0.
+It will restart them (gracefully and with timeout one by one).
+That will allow to reach zero downtime while we restarting the application with new codebase (everything in worker.js).
+
+P.S.: If you need to update primary.js as well, you need to shutdown whole application and run it again.
+`
+)
